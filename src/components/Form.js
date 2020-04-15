abcf1084633a52e733800fc0330a4fed41b295c8
Alouiscious Media <info@reneesoul.com>
2020-04-15 01:30:31 -0700

    abstracted onchange to one handler in parent

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,7 +11,7 @@ class Form extends React.Component {
       <div>
         <form>
           <input type="text"  name="firstName" onChange={event => this.props.handleChange(event)} value={this.props.formData.firstName}/>
-          <input type="text"  name="firstName" onChange={event => this.props.handleChange(event)} value={this.props.formData.lastName}/>
+          <input type="text"  name="lastName" onChange={event => this.props.handleChange(event)} value={this.props.formData.lastName}/>
           <input type="submit"/>
         </form>
       </div>
